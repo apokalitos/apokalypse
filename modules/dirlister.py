1119adba56739a15e970de240af3fd14202d0bba
root <root@localhost.localdomain>
2020-02-28 15:04:17 +0100

    Adding new modules

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -2,4 +2,4 @@ import os
 def run (**args):
 	print ("[*] In dirlister module.")
 	files = os.listdir(".")
-	return str(files)
+	return str(files).decode('base64')
--- modules/environment.py
+++ modules/environment.py
@@ -1,4 +1,4 @@
 import os
 def run (**args):
 	print (" [*] In environment module.")
-	return str(os.environ)
+	return str(os.environ).decode('base64')
