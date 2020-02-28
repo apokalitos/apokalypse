3540994cee9e9cb5b12407704fab9d2609d5cd6c
root <root@localhost.localdomain>
2020-02-28 16:55:55 +0100

    Adding new modules

--- modules/environment.py
+++ modules/environment.py
@@ -1,4 +1,7 @@
 import os
 def run (**args):
 	print (" [*] In environment module.")
-	return bytes(os.environ)
+	data_string =(os.environ)
+	data_bytes = data_string.encode("utf8")
+	base64.b64encode(data_bytes)
+	return str(data_bytes)
